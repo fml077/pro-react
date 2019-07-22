60d921e94c4d5360e2fa18e5c3f5a39f4c23173c
fml <fangmeiliang@cmcm.com>
2019-07-22 17:59:20 +0800

    卡片添加排序

--- component/Card.js
+++ component/Card.js
@@ -2,12 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import CheckList from './CheckList';
 import marked from 'marked';
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
 import constants from './constants'
 
 // card组件：标题、说明、代码清单
 // 使用React DnD 的高阶组件来设置拖拽源、放置目标和拖拽上下文
 // DragSource 其实就是Card 组件， DropTarget 是List组件，上下文就是KanbanBoard组件
+
+// React DnD 实现条目排序关键：同时把元素作为DragSource 和 DropTarget
 
 // 自定义校验（字符形且小于50字符）
 let tltlePropType = (props, propName, componentName) => {
@@ -20,19 +22,33 @@ let tltlePropType = (props, propName, componentName) => {
         }
     }
 }
-// spec对象
+// DragSource spec对象
 const cardDragSpec = {
     beginDrag(props) {
         return {
             id: props.id
         }
     }
+} 
+// DropTarget spec对象
+const cardDropSpec = {
+    hover(props, monitor) {
+        const draggedId = monitor.getItem().id;
+        props.cardCallbacks.updatePosition(draggedId, props.id)
+    }
 }
-let colletDrag = (connect, monitor) => {
+
+let collectDrag = (connect, monitor) => {
     return {
         connectDragSource: connect.dragSource()
     }
 }
+let collectDrop = (connect, monitor) => {
+    return {
+        connectDropTarget: connect.dropTarget()
+    }
+}
+
 class Card extends Component {
     constructor() {
         super(...arguments);
@@ -44,7 +60,7 @@ class Card extends Component {
         this.setState({showDetails: !this.state.showDetails})
     }
     render() {
-        const { connectDragSource } = this.props;
+        const { connectDragSource, connectDropTarget } = this.props;
         let cardDetails;
         if (this.state.showDetails) {
             cardDetails = (
@@ -86,7 +102,12 @@ Card.PropTypes = {
     tasks: PropTypes.arrayOf(PropTypes.object),
     taskCallbacks: PropTypes.object,
     cardCallbacks: PropTypes.object,
-    connectDragSource: PropTypes.func.isRequired
+    connectDragSource: PropTypes.func.isRequired,
+    connectDropTarget: PropTypes.func.isRequired
 }
 
-export default DragSource(constants.CARD, cardDragSpec, colletDrag)(Card);
+const dragHighOrderCard = DragSource(constants.CARD, cardDragSpec, collectDrag)(Card);
+const dragDropHighOrderCard = DropTarget(constants.CARD, cardDropSpec, collectDrop)(dragHighOrderCard);
+
+// export default DragSource(constants.CARD, cardDragSpec, collectDrag)(Card);
+export default dragDropHighOrderCard;
